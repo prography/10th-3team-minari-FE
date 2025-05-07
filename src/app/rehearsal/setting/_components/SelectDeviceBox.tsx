935d13c68edd3e459a0cc88a252ea4a115de5d11
_wo_.ong <ckehfqkr302@gmail.com>
2025-05-07 16:07:10 +0900

    feat: 디바이스 selectBox 컴포넌트 작성

---
+++ src/app/rehearsal/setting/_components/SelectDeviceBox.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import styles from './SelectDeviceBox.module.css';
+
+type DeviceType = 'videoInput' | 'audioInput';
+
+interface SelectDeviceBoxProps {
+  type: DeviceType;
+  deviceList: MediaDeviceInfo[] | undefined;
+  selected: MediaDeviceInfo | undefined;
+  onSelect: (type: DeviceType, device: MediaDeviceInfo) => void;
+}
+
+const SelectDeviceBox = ({type, deviceList, selected, onSelect}: SelectDeviceBoxProps) => {
+  const [open, setOpen] = useState(false);
+
+  const handleItemClick = (device: MediaDeviceInfo) => {
+    onSelect(type, device);
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <button onClick={() => setOpen((prev) => !prev)} className={styles.selected}>
+        {selected?.label ?? `${type === 'videoInput' ? '카메라' : '마이크'}를 선택하세요`} ▼
+      </button>
+      {open && (
+        <ul className={styles.options}>
+          {deviceList?.map((device) => (
+            <li key={device.deviceId} onClick={() => handleItemClick(device)}>
+              {device.label}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default SelectDeviceBox;
