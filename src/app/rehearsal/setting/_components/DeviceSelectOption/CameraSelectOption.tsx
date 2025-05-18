269a508b034a845097f82cd1175271bce0da33dc
_wo_.ong <ckehfqkr302@gmail.com>
2025-05-18 23:43:46 +0900

    feat: 카메라 SelectOption 컴포넌트

---
+++ src/app/rehearsal/setting/_components/DeviceSelectOption/CameraSelectOption.tsx
@@ -0,0 +1,37 @@
+import SelectOption from '@/components/SelectOption/SelectOption';
+import useMedia from '@/hooks/useMedia';
+import Image from 'next/image';
+import ChevronDown from '@/assets/icon/chevron-down.svg';
+import styles from './DeviceSelectOption.module.css';
+import Camera from '@/assets/icon/camera.svg';
+import {useDeviceSelect} from '@/contexts/DeviceSelectProvider';
+
+const CameraSelectOption = () => {
+  const {mediaStream} = useMedia();
+  const {device, handleCameraClick} = useDeviceSelect();
+
+  return (
+    <div className={styles.wrapper}>
+      <button className={styles.button} onClick={handleCameraClick}>
+        <div className={styles.icon}>
+          <Image src={Camera} alt="icon" width={24} height={24} />
+        </div>
+        <Image src={ChevronDown} alt="icon" width={16} height={16} />
+      </button>
+
+      {device === 'camera' && (
+        <div className={styles.setting}>
+          <SelectOption
+            label={<SelectOption.Label>카메라</SelectOption.Label>}
+            inputField={
+              <SelectOption.InputFieldDevics type="videoInput" disabled={mediaStream == null} />
+            }
+            options={<SelectOption.OptionsDevices type="videoInputDevices" />}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CameraSelectOption;
