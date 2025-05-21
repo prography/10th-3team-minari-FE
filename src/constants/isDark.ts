593d1291bb38834d9feceb6087dec81410c453a2
_wo_.ong <ckehfqkr302@gmail.com>
2025-05-22 02:21:20 +0900

    feat: 다크모드, 경로 상수 작성

---
+++ src/constants/isDark.ts
@@ -0,0 +1,3 @@
+import {PATH} from './path';
+
+export const IS_DARK = [PATH.REHEARSAL, PATH.REHEARSAL_SETTING];
---
+++ src/constants/path.ts
@@ -0,0 +1,10 @@
+const REHEARSAL = 'rehearsal';
+const SETTING = 'setting';
+const RESULT = 'result';
+
+export const PATH = {
+  ROOT: '/',
+  REHEARSAL: `/${REHEARSAL}`,
+  REHEARSAL_SETTING: `/${REHEARSAL}/${SETTING}`,
+  REHEARSAL_RESULT: `/${REHEARSAL}/${RESULT}`,
+};
