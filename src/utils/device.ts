d6c58453ca5b470e2d84b26ee38d03b8ab2c9b73
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-02 11:20:31 +0900

    refactor: 디바이스 예외처리 작업 진행

--- src/utils/device.ts
+++ src/utils/device.ts
@@ -1,5 +1,21 @@
 export const getDevices = async () => {
+  if (typeof window === 'undefined' || typeof navigator === 'undefined') {
+    console.warn('브라우저가 아닌 환경에서는 enumerateDevices를 실행하지 않습니다.');
+    return {
+      videoInputDevices: [],
+      audioInputDevices: [],
+    };
+  }
+
+  if (!navigator.mediaDevices || typeof navigator.mediaDevices.enumerateDevices !== 'function') {
+    console.warn('이 브라우저는 enumerateDevices를 지원하지 않습니다.');
+    return {
+      videoInputDevices: [],
+      audioInputDevices: [],
+    };
+  }
   const devices = await navigator.mediaDevices.enumerateDevices();
+
   const videoInputDevices = devices.filter((device) => device.kind === 'videoinput');
   const audioInputDevices = devices.filter((device) => device.kind === 'audioinput');
 
