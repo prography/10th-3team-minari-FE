b2a18661710679c312985251f4acd703332cc2fb
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-17 22:38:38 +0900

    refactor: 리허설과 리허설 세팅 페이지 이외에는 디바이스 정지

--- src/components/LayoutWrapper/index.tsx
+++ src/components/LayoutWrapper/index.tsx
@@ -1,17 +1,29 @@
 'use client';
 
-import {ReactNode} from 'react';
+import {ReactNode, useEffect} from 'react';
 import styles from './LayoutWrapper.module.css';
 import Header from '@/components/Header';
 import {usePathname} from 'next/navigation';
 import {useUserStore} from '@/stores/userStore';
 import useTheme from '@/hooks/useTheme';
 import {PATH} from '@/constants/path';
+import useMedia from '@/hooks/useMedia';
+import {useMediaStore} from '@/stores/mediaStore';
 
 const LayoutWrapper = ({children}: {children: ReactNode}) => {
   const pathname = usePathname();
   const userStore = useUserStore();
   const theme = useTheme();
+  const {stopMedia} = useMedia();
+  const {mediaStreamStatus} = useMediaStore();
+
+  useEffect(() => {
+    const isAllowedPath = pathname === '/rehearsal' || pathname === '/rehearsal/setting';
+
+    if (!isAllowedPath && mediaStreamStatus === 'connected') {
+      stopMedia();
+    }
+  }, [pathname, stopMedia, mediaStreamStatus]);
 
   // 랜딩 페이지는 LayoutWrapper 스타일 적용 x
   const avoidWrapper = pathname === PATH.ROOT && !userStore.isLoggedIn;
