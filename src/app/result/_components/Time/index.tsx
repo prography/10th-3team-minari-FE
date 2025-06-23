90bd344c2859ed274d7c7e7c1ba2e589de008500
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-24 01:50:12 +0900

    feat: 서버에서 날짜와 런타임 받아오늘 것으로 수정

--- src/app/result/_components/Time/index.tsx
+++ src/app/result/_components/Time/index.tsx
@@ -1,12 +1,11 @@
-'use client';
-
 import React from 'react';
 
-const Time = () => {
-  // const minutes = Math.floor(seconds / 60);
-  // const remainingSeconds = seconds % 60;
+const Time = ({runningTime}: {runningTime?: number}) => {
+  const second = Math.floor(runningTime != null ? runningTime : 0);
+  const minutes = Math.floor(second / 60);
+  const remainingSeconds = second % 60;
 
-  return <div>{/* {minutes}분 {remainingSeconds}초 */}</div>;
+  return <div>{`${minutes}분 ${remainingSeconds}초`}</div>;
 };
 
 export default Time;
