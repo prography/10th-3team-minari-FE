8ce6eea4691faafc3c872738e1244bf3999f8af3
Jiwon Choi <jiwon94k@gmail.com>
2025-06-26 05:24:04 +0900

    feat: [메인] 랜딩페이지 메뉴 수정
    - FAQ 삭제
    - 메뉴 링크 연결

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -8,3 +8,9 @@ export const PATH = {
   REHEARSAL_SETTING: `/${REHEARSAL}/${SETTING}`,
   REHEARSAL_RESULT: `/${REHEARSAL}/${RESULT}`,
 };
+
+export const OUT_LINK = {
+  FAQ: 'https://button-shear-b28.notion.site/MINARI-21b31f010d1f8059be25e11819c8097b',
+  BLOG: 'https://velog.io/@official_minari/posts',
+  AGREEMENTS: 'https://button-shear-b28.notion.site/MINARI-20131f010d1f8021839ae73b9e51b1f4',
+} as const;
