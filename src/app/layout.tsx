880328823e8364e4144b35610eda3c972d4f5bb7
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-26 19:32:13 +0900

    feat: 모달 클라이언트 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './common.css';
 import Providers from '@/contexts/Providers';
 import LayoutWrapper from '@/components/LayoutWrapper';
 import {GoogleAnalytics} from '@next/third-parties/google';
+import {ModalClient} from '@/components/Modal/ModalClient';
 
 export default function RootLayout({
   children,
@@ -11,10 +12,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0"
+        />
+      </head>
       <body>
         <Providers>
           <LayoutWrapper>{children}</LayoutWrapper>
         </Providers>
+        <ModalClient />
       </body>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID ? process.env.NEXT_PUBLIC_GA_ID : ''} />
     </html>
