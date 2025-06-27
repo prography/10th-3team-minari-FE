9a9ac5880e63c1e50c94c163e1392cdcaec48868
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-28 03:19:14 +0900

    feat: window.alert 로 만들어진 모달 커스텀 모달로 변경

--- src/app/users/oauth/kakao/page.tsx
+++ src/app/users/oauth/kakao/page.tsx
@@ -6,11 +6,15 @@ import {useRouter} from 'next/navigation';
 import Loader from '@/components/Loader';
 import {getKakaoProfile} from '@/apis/user';
 import {setCookie} from '@/utils/cookies';
+import Modal from '@/components/Modal';
+import {useModalStore} from '@/stores/modalStore';
+import Button from '@/components/Button';
 
 const KakaoRedirectPage = () => {
   let code = '';
   const store = useUserStore();
   const router = useRouter();
+  const {open} = useModalStore();
 
   // TODO Loading UI 확정 후 수정 & 예외처리 추가
   useEffect(() => {
@@ -38,8 +42,21 @@ const KakaoRedirectPage = () => {
       .catch((e) => {
         console.log(e, '카카오 로그인 에러 발생');
         localStorage.clear();
-        window.alert('로그인 실패');
-        router.push('/');
+        open(
+          <Modal
+            title="로그인 실패"
+            rightButton={
+              <Button
+                onClick={() => {
+                  router.push('/');
+                }}
+              >
+                확인
+              </Button>
+            }
+          />,
+          true,
+        );
       });
   }, [code]);
   return <Loader />;
