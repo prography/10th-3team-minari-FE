8436d868503ab12c559f1be30e303ad3e3ff5960
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-28 03:17:45 +0900

    feat: 모든 props 다 옵셔널로 수정

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,19 +2,19 @@ import styles from './Model.module.css';
 
 interface ModalProps {
   title?: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
   leftButton?: React.ReactNode;
-  rightButton: React.ReactNode;
+  rightButton?: React.ReactNode;
 }
 
 const Modal = ({title, children, leftButton, rightButton}: ModalProps) => {
   return (
     <div onClick={(e) => e.stopPropagation()} className={styles.modal}>
       {title != null && <span className="title-sm">{title}</span>}
-      <span className="body-lg">{children}</span>
+      {children != null && <span className="body-lg">{children}</span>}
       <div className={styles.buttons}>
         {leftButton != null && leftButton}
-        {rightButton}
+        {rightButton != null && rightButton}
       </div>
     </div>
   );
