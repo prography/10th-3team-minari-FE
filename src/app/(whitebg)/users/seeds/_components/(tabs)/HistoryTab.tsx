581f196680e09415a55f7e8e7557d815a0ab79f5
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-29 07:08:48 +0900

    refactor: 약관동의 상수 추가

--- src/app/(whitebg)/users/seeds/_components/(tabs)/HistoryTab.tsx
+++ src/app/(whitebg)/users/seeds/_components/(tabs)/HistoryTab.tsx
@@ -1,3 +1,5 @@
+import {NOTE_HISTORY} from '@/constants/note';
+import Note from '../Note';
 import Header from './(table)/Header';
 import Title from './(table)/Title';
 
@@ -6,6 +8,7 @@ const HistoryTab = () => {
     <>
       <Title>씨앗 구입 내역</Title>
       <Header ths={['날짜', '씨앗', '갯수', '상태', '거래내역서 발급']} />
+      <Note NoteData={NOTE_HISTORY} />
     </>
   );
 };
