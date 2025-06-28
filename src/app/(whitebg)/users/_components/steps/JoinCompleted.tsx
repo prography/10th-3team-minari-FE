29e137c2d56dc24800f556fc2973451f5f7dec1e
Jiwon Choi <jiwon94k@gmail.com>
2025-06-29 05:23:38 +0900

    feat: [마이페이지] 화면개발
    - 프로필 카드 컴포넌트 분리

--- src/app/(whitebg)/users/_components/steps/JoinCompleted.tsx
+++ src/app/(whitebg)/users/_components/steps/JoinCompleted.tsx
@@ -1,8 +1,7 @@
 import styles from './JoinCompleted.module.css';
 import Image from 'next/image';
-import ProfileMinari from '@/assets/icon/profile-minari.svg';
 import GrayMinari from '@/assets/minari-gray-cut.svg';
-import Pen from '@/assets/icon/pen-gray.png';
+import ProfileCard from '@/app/users/_components/my/ProfileCard';
 
 interface JoinCompletedProps {
   name: string;
@@ -14,22 +13,7 @@ const JoinCompleted = ({name, domain, email}: JoinCompletedProps) => {
   const selectedDomain = domain === 'FRONTEND' ? '프론트엔드' : '백엔드';
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className="fx">
-          <Image src={ProfileMinari} alt="" />
-          <div className={styles['text-section']}>
-            <div className={styles['title__wrap']}>
-              <div className="label-lg txt-white">{name}님</div>
-              <div className={styles['edit-profile__button']}>
-                <Image src={Pen} alt="" />
-                <span className="label-md txt-inverse-gray">프로필 수정</span>
-              </div>
-            </div>
-            <div className="body-md">{selectedDomain} | 미나리 0일차</div>
-            <div>{email}</div>
-          </div>
-        </div>
-      </div>
+      <ProfileCard name={name} domain={selectedDomain} email={email} />
       <div className={styles['bg-image__wrapper']}>
         <Image src={GrayMinari} alt="" />
       </div>
---
+++ src/app/users/_components/my/ProfileCard/index.tsx
@@ -0,0 +1,32 @@
+import styles from '@/app/users/_components/join/JoinCompleted.module.css';
+import Image from 'next/image';
+import ProfileMinari from '@/assets/icon/profile-minari.svg';
+import Pen from '@/assets/icon/pen-gray.png';
+
+interface ProfileCardProps {
+  name: string | undefined;
+  domain: string | undefined;
+  email: string | undefined;
+}
+const ProfileCard = ({name, domain, email}: ProfileCardProps) => {
+  return (
+    <div className={styles.container}>
+      <div className="fx">
+        <Image src={ProfileMinari} alt="" />
+        <div className={styles['text-section']}>
+          <div className={styles['title__wrap']}>
+            <div className="label-lg txt-white">{name}님</div>
+            <div className={styles['edit-profile__button']}>
+              <Image src={Pen} alt="" />
+              <span className="label-md txt-inverse-gray">프로필 수정</span>
+            </div>
+          </div>
+          <div className="body-md">{domain} | 미나리 0일차</div>
+          <div>{email}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
