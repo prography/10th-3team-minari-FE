cb872e94e37380349400500c87d5bb9cc97f283c
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-28 20:24:00 +0900

    refactor: 공통으로 뺀 텝 컴포넌트 사용 & 스타일 수정

--- src/app/users/seeds/_components/(tabs)/TabView/index.tsx
+++ src/app/users/seeds/_components/(tabs)/TabView/index.tsx
@@ -1,33 +1,33 @@
 'use client';
 
 import {useRouter} from 'next/navigation';
-import styles from './TabView.module.css';
 import {useCurrentParams} from '@/hooks/useCurrentParams';
-import TabButton from '../../TabButton';
 import BuyTab from '../BuyTab';
 import HistoryTab from '../HistoryTab';
 import UsageTab from '../UsageTab';
+import TabButtonWrapper from '@/components/TabButton/TabButtonWrapper';
+import TabButton from '@/components/TabButton';
 
 const TabView = () => {
   const router = useRouter();
   const tab = useCurrentParams('tabs', 'buy');
 
   return (
-    <div style={{marginTop: 16}}>
-      <nav className={styles.nav_list}>
+    <>
+      <TabButtonWrapper>
         {['buy', 'history', 'usage'].map((t) => (
           <TabButton key={t} onClick={() => router.push(`?tabs=${t}`)} active={tab === t}>
             {t === 'buy' ? '씨앗 사기' : t === 'history' ? '구입 내역' : '사용 내역'}
           </TabButton>
         ))}
-      </nav>
+      </TabButtonWrapper>
 
-      <div style={{marginTop: 32}}>
+      <div>
         {tab === 'buy' && <BuyTab />}
         {tab === 'history' && <HistoryTab />}
         {tab === 'usage' && <UsageTab />}
       </div>
-    </div>
+    </>
   );
 };
 
