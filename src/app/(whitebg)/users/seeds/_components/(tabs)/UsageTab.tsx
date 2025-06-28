de4c68993608002b27cd1ece40fcf44e6704a6bf
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-29 07:08:48 +0900

    refactor: 약관동의 상수 추가

--- src/app/(whitebg)/users/seeds/_components/(tabs)/UsageTab.tsx
+++ src/app/(whitebg)/users/seeds/_components/(tabs)/UsageTab.tsx
@@ -1,3 +1,5 @@
+import {NOTE_USAGE} from '@/constants/note';
+import Note from '../Note';
 import Header from './(table)/Header';
 import Title from './(table)/Title';
 
@@ -6,6 +8,7 @@ const UsageTab = () => {
     <>
       <Title>씨앗 사용 내역</Title>
       <Header ths={['날짜', '씨앗', '갯수', '상태', '환불']} />
+      <Note NoteData={NOTE_USAGE} />
     </>
   );
 };
