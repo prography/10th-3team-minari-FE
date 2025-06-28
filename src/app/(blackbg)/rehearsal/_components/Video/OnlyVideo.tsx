1ea96c9ed59987828aabdfbd9de603be6f813de8
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-29 04:27:31 +0900

    refactor: background 색상 다크인 애들 모음

---
+++ src/app/(blackbg)/rehearsal/_components/Video/OnlyVideo.tsx
@@ -0,0 +1,34 @@
+import {useMediaStore} from '@/stores/mediaStore';
+import React, {useEffect, useRef} from 'react';
+import styles from './Video.module.css';
+import useMedia from '@/hooks/useMedia';
+import {useDeviceStore} from '@/stores/devicsStore';
+
+const OnlyVideo = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const {mediaStream} = useMediaStore();
+  const {mediaStreamStatus, startMedia} = useMedia();
+  const {selectDevice} = useDeviceStore();
+
+  const {videoInput, audioInput} = selectDevice;
+
+  useEffect(() => {
+    if (mediaStreamStatus === 'idle' || mediaStreamStatus === 'pending') {
+      void startMedia({videoInput, audioInput}, true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.srcObject = mediaStream;
+    }
+  }, [mediaStream]);
+
+  return (
+    <div className={styles.video_wrapper}>
+      <video ref={videoRef} autoPlay muted playsInline />
+    </div>
+  );
+};
+
+export default OnlyVideo;
