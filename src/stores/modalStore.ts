ebb202e9cc3cbcbdda694ad100f62096e2ed453f
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-28 16:58:32 +0900

    refactor: backdrop 클릭 여부 open에서 조건 추가

--- src/stores/modalStore.ts
+++ src/stores/modalStore.ts
@@ -3,15 +3,17 @@ import {create} from 'zustand';
 interface ModalStore {
   modal: React.ReactNode | null;
   isOpen: boolean;
-  open: (modal: React.ReactNode) => void;
+  open: (modal: React.ReactNode, disableBackdropClick: boolean) => void;
   close: () => void;
   change: (modal: React.ReactNode) => void;
+  disableBackdropClick: boolean;
 }
 
 export const useModalStore = create<ModalStore>((set) => ({
   modal: null,
   isOpen: false,
-  open: (modal) => set({isOpen: true, modal}),
-  close: () => set({isOpen: false, modal: null}),
+  open: (modal, disableBackdropClick = false) => set({isOpen: true, modal, disableBackdropClick}),
+  close: () => set({isOpen: false, modal: null, disableBackdropClick: false}),
   change: (modal) => set((state) => (state.isOpen ? {...state, modal} : state)),
+  disableBackdropClick: false,
 }));
