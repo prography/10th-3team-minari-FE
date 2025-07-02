4318db7cf4145da9d7f18e26f70af6b9eccc8696
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-02 10:03:58 +0900

    feat: 외부 경로 추가

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -15,6 +15,8 @@ export const PATH = {
 export const OUT_LINK: {[key: string]: string} = {
   FAQ: 'https://button-shear-b28.notion.site/MINARI-21b31f010d1f8059be25e11819c8097b',
   BLOG: 'https://velog.io/@official_minari/posts',
-  AGREEMENTS_1: 'https://button-shear-b28.notion.site/MINARI-20131f010d1f807b89ece82517780def',
-  AGREEMENTS_2: 'https://button-shear-b28.notion.site/MINARI-20131f010d1f8069b258c93a49c44675',
+  개인정보_취급방침: 'https://button-shear-b28.notion.site/MINARI-20131f010d1f807b89ece82517780def',
+  이용약관: 'https://button-shear-b28.notion.site/MINARI-20131f010d1f8069b258c93a49c44675',
+  환불_정책: 'https://button-shear-b28.notion.site/21b31f010d1f8077b4f6e57235b52079',
+  공지사항: 'https://button-shear-b28.notion.site/22331f010d1f80538f24cb456a7f6eff',
 } as const;
