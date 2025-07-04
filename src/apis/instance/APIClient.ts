8dc617c38f742ec5522e161d2c7bbf1ae9122c84
Jiwon Choi <jiwon94k@gmail.com>
2025-07-05 01:27:44 +0900

    fix: token 변수명 변경

--- src/apis/instance/APIClient.ts
+++ src/apis/instance/APIClient.ts
@@ -2,6 +2,8 @@ import {getCookie} from '@/utils/cookies';
 import {ErrorMessage} from '@/apis/instance/type';
 import {redirect} from 'next/navigation';
 
+import {getCookie} from '@/utils/cookies';
+
 export type ApiResponse<T> = {
   code: string;
   result: T | null;
