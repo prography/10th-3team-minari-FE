c08f8d97d6a8890289012be5d127b68960ef8e0c
Jiwon Choi <jiwon94k@gmail.com>
2025-07-06 10:42:13 +0900

    feat: [마이페이지] 어드민 버튼 추가 / 모달 추가
    - 탈퇴 여부 재확인 모달 추가
    - 탈퇴 후 toast안내 -> 모달 안내로 변경

--- src/app/(graybg)/users/_components/my/Tab/More/index.tsx
+++ src/app/(graybg)/users/_components/my/Tab/More/index.tsx
@@ -3,41 +3,64 @@ import styles from './More.module.css';
 import {deleteUser} from '@/apis/user';
 import Toast from '@/components/Toast';
 import {useToastStore} from '@/stores/toastStore';
-import {deleteCookie} from '@/utils/cookies';
+import Button from '@/components/Button';
+import ArrowRight from '@/assets/icon/arrow-black.svg';
+import {useRouter} from 'next/navigation';
+import {useModalStore} from '@/stores/modalStore';
+import React from 'react';
+import Modal from '@/components/Modal';
+import {useClearCache} from '@/hooks/useClearCache';
 
 const MoreTab = () => {
   const toastStore = useToastStore();
-  const navigateHome = () => {
-    window.location.href = '/';
+  const router = useRouter();
+  const {open, close} = useModalStore();
+  const {clearCookies, goHome} = useClearCache();
+  const allClear = async () => {
+    await clearCookies();
+    goHome();
   };
 
+  // 로그아웃
   const onClickLogout = async () => {
     toastStore.setTime(1000);
     toastStore.open(<Toast title={'로그아웃 되었어요.'} />);
-    setTimeout(async () => {
-      localStorage.removeItem('user-storage');
-      await deleteCookie('access-token');
-      await deleteCookie('refresh-token');
-      navigateHome();
+    setTimeout(() => {
+      allClear();
     }, 800);
   };
+
+  // 탈퇴 클릭 >> 확인 모달
   const onClickWithdraw = async () => {
+    open(
+      <Modal
+        title="정말 계정을 탈퇴 하시겠어요?"
+        leftButton={
+          <Button onClick={close} theme="secondary">
+            취소
+          </Button>
+        }
+        rightButton={<Button onClick={fetchDeleteUser}>계속</Button>}
+      >
+        <p>가지고 있는 모든 혜택이 사라져요.</p>
+        <p>진행 이후 7일 이내에 복구가 가능해요.</p>
+      </Modal>,
+    );
+  };
+
+  // 탈퇴
+  const fetchDeleteUser = async () => {
     await deleteUser()
       .then((response) => {
         if (response?.code === '200') {
-          toastStore.setTime(2000);
-          toastStore.open(
-            <Toast
-              title={'탈퇴가 완료되었어요'}
-              description="그동안 미나리를 사랑해주셔서 감사합니다."
-            />,
+          open(
+            <Modal
+              title="탈퇴가 정상적으로 완료되었습니다"
+              rightButton={<Button onClick={allClear}>처음으로</Button>}
+            >
+              <p>그동안 미나리를 이용해주셔서 감사합니다.</p>
+            </Modal>,
           );
-          setTimeout(async () => {
-            localStorage.removeItem('user-storage');
-            await deleteCookie('access-token');
-            await deleteCookie('refresh-token');
-            navigateHome();
-          }, 1500);
         }
       })
       .catch(() => {
@@ -47,6 +70,7 @@ const MoreTab = () => {
         );
       });
   };
+
   return (
     <div className={styles['button-section']}>
       <button className="body-lg txt-disabled" onClick={onClickLogout}>
@@ -55,6 +79,9 @@ const MoreTab = () => {
       <button className="body-lg txt-disabled" onClick={onClickWithdraw}>
         회원탈퇴
       </button>
+      <Button iconRight={ArrowRight} onClick={() => router.push('/admin')}>
+        어드민
+      </Button>
     </div>
   );
 };
