aab3e74de22c6f060cb2b1099fdcfe615c06c9f8
Jiwon Choi <jiwon94k@gmail.com>
2025-07-06 15:45:04 +0900

    refactor: [회원가입] 파일 경로 수정

--- src/app/(whitebg)/users/join/_components/JoinCompleted.tsx
+++ src/app/(whitebg)/users/join/_components/JoinCompleted.tsx
@@ -1,7 +1,7 @@
 import styles from './JoinCompleted.module.css';
 import Image from 'next/image';
 import GrayMinari from '@/assets/minari-gray-cut.svg';
-import ProfileCard from '@/app/(graybg)/users/_components/my/ProfileCard';
+import ProfileCard from 'src/app/(graybg)/users/_components/ProfileCard';
 
 interface JoinCompletedProps {
   name: string;
