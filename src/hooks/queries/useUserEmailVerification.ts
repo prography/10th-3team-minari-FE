ba692d24f55c71118a3f225a44fa476d51463592
Jiwon Choi <jiwon94k@gmail.com>
2025-07-06 20:19:54 +0900

    refactor: [회원가입] 함수명 변경

--- src/hooks/queries/useUserEmailVerification.ts
+++ src/hooks/queries/useUserEmailVerification.ts
@@ -1,29 +1,29 @@
 import {useQuery} from '@tanstack/react-query';
 import {postEmailVerification} from '@/apis/user';
-import {useState} from 'react';
+
+interface UseUserEmailVerificationProps {
+  email: string;
+}
 
 // 이메일 인증번호 전송
-export const useUserEmailVerification = (email: string) => {
-  const [isError, setIsError] = useState<boolean>(false);
-
-  const refetchEmailVerification = async () => {
+export const useUserEmailVerification = ({email}: UseUserEmailVerificationProps) => {
+  const fetchEmailVerification = async () => {
     const response = await postEmailVerification(email);
-    if (response?.code === '200') {
-      setIsError(false);
-    } else {
-      setIsError(true);
-    }
     return response;
   };
 
-  useQuery({
+  const {data, isFetching, isLoading, refetch, isError} = useQuery({
     queryKey: ['user-email-verification'],
-    queryFn: refetchEmailVerification,
+    queryFn: fetchEmailVerification,
     enabled: false,
   });
 
   return {
-    refetchEmailVerification,
+    data,
     isError,
+    isFetching,
+    isLoading,
+    refetch,
+    fetchEmailVerification,
   };
 };
