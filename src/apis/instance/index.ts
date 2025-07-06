82539db24d25ab39e6818a27f70d0f2ca327fc1b
Jiwon Choi <jiwon94k@gmail.com>
2025-07-06 20:15:25 +0900

    feat: [공통] 어드민용 fetch instance 추가

--- src/apis/instance/index.ts
+++ src/apis/instance/index.ts
@@ -1,13 +1,25 @@
 import {apiClient} from './APIClient';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL_ADMIN = process.env.NEXT_PUBLIC_API_URL_ADMIN;
 
 if (!API_BASE_URL) {
+  throw new Error('API_BASE_URL is not defined');
+}
+if (!API_BASE_URL_ADMIN) {
   throw new Error('API_BASE_URL is not defined');
 }
 
 export const fetch = apiClient.create({
   baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    credentials: 'include',
+  },
+});
+
+export const fetchAdmin = apiClient.create({
+  baseURL: API_BASE_URL_ADMIN,
   headers: {
     'Content-Type': 'application/json',
     credentials: 'include',
