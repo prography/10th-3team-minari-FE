b1b4269d00b060019c4dcafb4f3057327430441d
Jiwon Choi <jiwon94k@gmail.com>
2025-07-06 20:17:06 +0900

    feat: [어드민] api 연동 수정

---
+++ src/constants/adminOptions.ts
@@ -0,0 +1,10 @@
+export const ROLE_OPTIONS = [
+  {id: 0, option: 'PO'},
+  {id: 1, option: 'FE'},
+  {id: 2, option: 'BE'},
+];
+export const REASON_OPTIONS = [
+  {id: 0, option: '입금 인증'},
+  {id: 1, option: '시스템 오류'},
+  {id: 2, option: '기타 (직접 입력)'},
+];
