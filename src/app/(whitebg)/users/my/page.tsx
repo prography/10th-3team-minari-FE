b8b3dcb72b6bedba5640e26c3069fedc11be2b68
Jiwon Choi <jiwon94k@gmail.com>
2025-07-09 01:41:49 +0900

    fix: 빌드에러 수정

--- src/app/(whitebg)/users/my/page.tsx
+++ src/app/(whitebg)/users/my/page.tsx
@@ -7,30 +7,26 @@ import {useCurrentParams} from '@/hooks/useCurrentParams';
 import {useRouter} from 'next/navigation';
 import InfoTab from '@/app/(whitebg)/users/my/_components/Tab/Info';
 import {UserHeatmapProvider} from '@/contexts/UserHeatmapProvider';
-import {Suspense} from 'react';
-import Loader from '@/components/Loader';
 
 const MyPage = () => {
   const tab = useCurrentParams('tabs', 'buy');
   const router = useRouter();
 
   return (
-    <Suspense fallback={<Loader />}>
-      <div className={styles.container}>
-        <TabButtonWrapper>
-          {['info', 'more'].map((t) => (
-            <TabButton key={t} onClick={() => router.push(`?tabs=${t}`)} active={tab === t}>
-              {t === 'info' ? '마이페이지' : '더보기'}
-            </TabButton>
-          ))}
-        </TabButtonWrapper>
+    <div className={styles.container}>
+      <TabButtonWrapper>
+        {['info', 'more'].map((t) => (
+          <TabButton key={t} onClick={() => router.push(`?tabs=${t}`)} active={tab === t}>
+            {t === 'info' ? '마이페이지' : '더보기'}
+          </TabButton>
+        ))}
+      </TabButtonWrapper>
 
-        <UserHeatmapProvider>
-          {tab === 'info' && <InfoTab />}
-          {tab === 'more' && <MoreTab />}
-        </UserHeatmapProvider>
-      </div>
-    </Suspense>
+      <UserHeatmapProvider>
+        {tab === 'info' && <InfoTab />}
+        {tab === 'more' && <MoreTab />}
+      </UserHeatmapProvider>
+    </div>
   );
 };
 
