9b080c5f891d2fd1910d24327c06d4599d46390a
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-08 12:35:57 +0900

    refactor: 연결상태일때만 리허설 페이지로 이동하는 버튼 활성화

--- src/app/(blackbg)/rehearsal/_components/Setting/_components/OptionGroup/index.tsx
+++ src/app/(blackbg)/rehearsal/_components/Setting/_components/OptionGroup/index.tsx
@@ -5,9 +5,19 @@ import ArrowBlack from '@/assets/icon/arrow-black.svg';
 import styles from './OptionGroup.module.css';
 import {DeviceSelectProvider} from '@/contexts/DeviceSelectProvider';
 import {useRehearsal} from '@/contexts/RehearsalProvider';
+import {useEffect, useState} from 'react';
+import {useMediaStore} from '@/stores/mediaStore';
 
 const OptionGroup = () => {
   const {handleIsReharsal} = useRehearsal();
+  const {mediaStreamStatus} = useMediaStore();
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    if (mediaStreamStatus === 'connected') {
+      setDisabled(false);
+    }
+  }, [mediaStreamStatus]);
 
   const handleRehearsalStart = () => {
     handleIsReharsal();
@@ -21,7 +31,7 @@ const OptionGroup = () => {
           <MicSelectOption />
         </DeviceSelectProvider>
       </div>
-      <Button onClick={handleRehearsalStart} iconLeft={ArrowBlack}>
+      <Button onClick={handleRehearsalStart} iconLeft={ArrowBlack} disabled={disabled}>
         면접 시작하기
       </Button>
     </div>
