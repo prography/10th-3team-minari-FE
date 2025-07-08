f46f202b4b17cf20ee01b3fe54f9180af74ce8ec
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-08 12:35:30 +0900

    feat: 모달 사이즈, 위치 받을 수 있도록 추가

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -5,11 +5,23 @@ interface ModalProps {
   children?: React.ReactNode;
   leftButton?: React.ReactNode;
   rightButton?: React.ReactNode;
+  position?: 'center' | 'top';
+  size?: 'sm' | 'md' | 'lg';
 }
 
-const Modal = ({title, children, leftButton, rightButton}: ModalProps) => {
+const Modal = ({
+  title,
+  children,
+  leftButton,
+  rightButton,
+  position = 'center',
+  size = 'lg',
+}: ModalProps) => {
   return (
-    <div onClick={(e) => e.stopPropagation()} className={styles.modal}>
+    <div
+      onClick={(e) => e.stopPropagation()}
+      className={`${styles.modal} ${position && styles[position]} ${size && styles[size]}`}
+    >
       {title != null && <span className="title-sm">{title}</span>}
       {children != null && <span className="body-lg">{children}</span>}
       <div className={styles.buttons}>
