8c8930044b20bf11edb8b7e79c12775e4c35a041
Jiwon Choi <jiwon94k@gmail.com>
2025-07-11 02:19:14 +0900

    fix: not-found 페이지 use client 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,18 @@
+'use client';
 import ErrorLayout from '@/components/ErrorLayout';
 import Button from '@/components/Button';
 
 export default function NotFound() {
-  const goHome = () => {
-    window.location.href = '/';
-  };
-
-  const goBack = () => {
-    window.history.back();
-  };
   return (
     <ErrorLayout code={404}>
       <div className="txt-secondary title-sm">앗.. 요청하신 페이지를 찾을 수 없습니다.</div>
       <div className="txt-secondary title-sm">불편을 드려 죄송합니다.</div>
 
       <div className="fx-align-center mg-top-20" style={{gap: '16px'}}>
-        <Button theme="secondary" border onClick={goBack}>
+        <Button theme="secondary" border onClick={() => window.history.back()}>
           이전 페이지
         </Button>
-        <Button border onClick={goHome}>
+        <Button border onClick={() => (window.location.href = '/')}>
           메인 페이지
         </Button>
       </div>
