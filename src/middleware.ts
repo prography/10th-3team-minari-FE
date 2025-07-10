f1e6a74c38c246627605288e8ea36ece0b85ef96
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-10 13:06:22 +0900

    refactor: 미들웨어에서 result 경로 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
 
   const {pathname} = request.nextUrl;
 
-  const protectedPaths = ['/rehearsal', '/result', '/minari'];
+  const protectedPaths = ['/rehearsal', '/minari'];
 
   if (protectedPaths.some((path) => pathname.includes(path)) && !token) {
     return NextResponse.rewrite(new URL('/401', request.url));
@@ -15,5 +15,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/rehearsal/:path*', '/result/:path*', '/minari/:path*'],
+  matcher: ['/rehearsal/:path*', '/minari/:path*'],
 };
