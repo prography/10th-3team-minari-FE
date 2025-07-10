312d1ca65f931c61f9880323f9e9e1f1c0898b77
Jiwon Choi <jiwon94k@gmail.com>
2025-07-11 02:49:38 +0900

    feat: [홈화면] 화면 변경내용 퍼블리싱

--- src/app/(whitebg)/minari/page.tsx
+++ src/app/(whitebg)/minari/page.tsx
@@ -6,6 +6,9 @@ import Image from 'next/image';
 import Button from '@/components/Button';
 import {useTag} from '@/hooks/queries/useTag';
 import {useQuestionId} from '@/hooks/queries/useQuestionId';
+import Shape from '@/assets/icon/shapes.svg';
+import Tag from '@/assets/icon/tag.svg';
+import ArrowRight from '@/assets/icon/arrow-black.svg';
 
 const MinariPage = () => {
   const {data: questionId} = useQuestionId();
@@ -17,22 +20,32 @@ const MinariPage = () => {
       <div className="title-lg">
         오늘의 미나리 <Image src={Minari} alt="" className="mg-top-4" width={28} />
       </div>
-      <div className="title-xs mg-top-8 txt-tertiary">
-        나에게 맞는 미나리를 선택하고, 풀어보세요!
-      </div>
+      <div className="title-xs mg-top-8 txt-secondary">지금 나에게 맞는 질문을 추천해드려요.</div>
       <div className={styles['category__container']}>
-        <div className="title-sm">{CATEGORY}</div>
+        <div>
+          <Image src={Shape} alt="shapes" />
+          <span className="label-lg txt-tertiary mg-left-4">세부 카테고리</span>
+        </div>
+        <div className={`label-lg txt-secondary ${styles.category}`}>{CATEGORY}</div>
       </div>
       <div className={styles['keyword__wrapper']}>
-        {data?.result?.map((item, i) => (
-          <div key={i} className={styles['keyword__container']}>
-            <span className="label-lg">{item}</span>
-          </div>
-        ))}
+        <div>
+          <Image src={Tag} alt="tag" />
+          <span className="label-lg txt-tertiary mg-left-4">관련 키워드</span>
+        </div>
+        <div className={styles['keyword__container']}>
+          {data?.result?.map((item, i) => (
+            <div key={i} className={styles.keyword}>
+              <span className="label-lg">{item}</span>
+            </div>
+          ))}
+        </div>
       </div>
       <div className={styles['button__wrapper']}>
         <a href="/rehearsal" target="_blank" rel="noopener noreferrer">
-          <Button rounded>면접 시작하기</Button>
+          <Button border iconRight={ArrowRight}>
+            면접 시작하기
+          </Button>
         </a>
       </div>
     </div>
