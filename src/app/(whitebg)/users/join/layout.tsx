bca821c2ee2276b38d6fb87383158bd8de7c1e7f
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-10 18:27:55 +0900

    feat: 페이지에 맞는 제목 추가

--- src/app/(whitebg)/users/join/layout.tsx
+++ src/app/(whitebg)/users/join/layout.tsx
@@ -1,5 +1,11 @@
 import {UserJoinProvider} from '@/contexts/UserJoinProvider';
 import styles from './layout.module.css';
+import type {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: '미나리 가입',
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
