de7e2fa92dbd096c55fe64625bd112234a47cb75
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-10 15:04:23 +0900

    feat: 아이콘 버튼 사이즈 추가

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,16 +1,25 @@
 import styles from './IconButton.module.css';
 import Image, {type StaticImageData} from 'next/image';
 
+type sizeType = 'size-40' | 'size-48';
+
 interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   theme?: 'primary' | 'secondary' | 'white';
   icon: StaticImageData;
   loading?: boolean;
+  size?: sizeType;
 }
 
-const IconButton = ({icon, theme = 'primary', loading, ...restProps}: IconButtonProps) => {
+const IconButton = ({
+  icon,
+  theme = 'primary',
+  loading,
+  size = 'size-48',
+  ...restProps
+}: IconButtonProps) => {
   return (
     <button
-      className={`${styles.button} ${styles[theme]} ${loading ? styles.rotation : ''}`}
+      className={`${styles.button} ${styles[theme]} ${loading ? styles.rotation : ''} ${styles[size]}`}
       {...restProps}
     >
       {icon != null ? <Image src={icon} alt="icon" width={24} height={24} /> : null}
