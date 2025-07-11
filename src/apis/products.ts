f5fd20e0746d3dc7a3c31c048ae80c4d1b21df04
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-11 23:43:22 +0900

    feat: 상품 히스토리 API 추가

--- src/apis/products.ts
+++ src/apis/products.ts
@@ -13,3 +13,16 @@ export const getProductsSell = async () => {
   const response = await fetch.get<ProductsSellType[]>('/products/sell');
   return response;
 };
+
+interface ProductsHistoryType {
+  date: string;
+  quantity: number;
+  category: 'EVENT' | 'BUY';
+  status: 'DONE' | 'HOLDING';
+  refund: boolean;
+}
+
+export const getProductsHistory = async () => {
+  const response = await fetch.get<ProductsHistoryType[]>('/products/history');
+  return response;
+};
