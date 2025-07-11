a4701eb0bf7cce771551ed7046e4b24d222cf0c9
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-12 01:12:40 +0900

    refactor: 필드 수정 및 반영

--- src/app/(whitebg)/users/seeds/_components/(tabs)/HistoryTab.tsx
+++ src/app/(whitebg)/users/seeds/_components/(tabs)/HistoryTab.tsx
@@ -22,12 +22,8 @@ const HistoryTab = () => {
             {type: 'text', text: `${value.date}`},
             {type: 'text', text: '씨앗'},
             {type: 'text', text: `${value.quantity}개`},
+            {type: 'text', text: `${value.remain}개`},
             {type: 'text', text: `${value.category === 'BUY' ? '구매' : '이벤트'}`},
-            {
-              type: 'badge',
-              text: `${value.status === 'DONE' ? '사용 완료' : '사용 대기'}`,
-              disabled: value.status === 'DONE',
-            },
             {
               type: 'button',
               text: '환불',
@@ -44,7 +40,7 @@ const HistoryTab = () => {
   return (
     <>
       <Title>씨앗 히스토리</Title>
-      <Header ths={['날짜', '종류', '갯수', '유형', '상태', '환불 여부']} />
+      <Header ths={['날짜', '종류', '구매수량', '남은 개수', '유형', '환불 여부']} />
       {TbBuyDatasMake().map((tbs, idx) => (
         <Body key={idx} tbs={tbs} />
       ))}
