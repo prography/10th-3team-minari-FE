099eaa692f35a86b271957696686e910de4f5758
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-11 17:05:32 +0900

    feat: 미나리 페이지 버튼 분기처리 진행

--- src/app/(whitebg)/minari/_components/MainButton/index.tsx
+++ src/app/(whitebg)/minari/_components/MainButton/index.tsx
@@ -1,15 +1,31 @@
 'use client';
+
 import ArrowRight from '@/assets/icon/arrow-black.svg';
 import styles from './MainButton.module.css';
-
 import Button from '@/components/Button';
+import {useAnswerEligibility} from '@/hooks/queries/useAnswerEligibility';
+import {useRouter} from 'next/navigation';
+import {PATH} from '@/constants/path';
 
 const MainButton = () => {
+  const {data} = useAnswerEligibility();
+  const router = useRouter();
+
   return (
     <div className={styles['button__wrapper']}>
+      {(data === 'LIMIT_REACHED' || data === 'SEED_REQUIRED') && (
+        <Button border theme="secondary" onClick={() => router.push(PATH.REHEARSAL_RESULT)}>
+          결과보기
+        </Button>
+      )}
       <a href="/rehearsal" target="_blank" rel="noopener noreferrer">
-        <Button border iconRight={ArrowRight}>
-          면접 시작하기
+        <Button
+          border
+          iconRight={ArrowRight}
+          disabled={data === 'UNKNOWN' || data === 'SEED_REQUIRED'}
+          full
+        >
+          {data === 'LIMIT_REACHED' ? '다시 도전하기' : '면접 시작하기'}
         </Button>
       </a>
     </div>
