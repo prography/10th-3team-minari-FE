af89aa2d5efa8fada122f8158ae661e59300ea61
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-11 22:51:38 +0900

    feat: 히스토리 테이블 문구 수정

--- src/app/(whitebg)/users/seeds/_components/(tabs)/HistoryTab.tsx
+++ src/app/(whitebg)/users/seeds/_components/(tabs)/HistoryTab.tsx
@@ -6,8 +6,8 @@ import Title from './(table)/Title';
 const HistoryTab = () => {
   return (
     <>
-      <Title>씨앗 구입 내역</Title>
-      <Header ths={['날짜', '씨앗', '갯수', '상태', '거래내역서 발급']} />
+      <Title>씨앗 히스토리</Title>
+      <Header ths={['날짜', '종류', '갯수', '유형', '상태', '환불 여부']} />
       <Note NoteData={NOTE_HISTORY} />
     </>
   );
--- src/app/(whitebg)/users/seeds/_components/(tabs)/TabView/index.tsx
+++ src/app/(whitebg)/users/seeds/_components/(tabs)/TabView/index.tsx
@@ -3,7 +3,6 @@
 import {useRouter} from 'next/navigation';
 import {useCurrentParams} from '@/hooks/useCurrentParams';
 import HistoryTab from '../HistoryTab';
-import UsageTab from '../UsageTab';
 import TabButtonWrapper from '@/components/TabButton/TabButtonWrapper';
 import TabButton from '@/components/TabButton';
 import BuyTab from '../BuyTab';
@@ -15,9 +14,9 @@ const TabView = () => {
   return (
     <>
       <TabButtonWrapper>
-        {['buy', 'history', 'usage'].map((t) => (
+        {['buy', 'history'].map((t) => (
           <TabButton key={t} onClick={() => router.push(`?tabs=${t}`)} active={tab === t}>
-            {t === 'buy' ? '씨앗 사기' : t === 'history' ? '구입 내역' : '사용 내역'}
+            {t === 'buy' ? '씨앗 사기' : '히스토리'}
           </TabButton>
         ))}
       </TabButtonWrapper>
@@ -25,7 +24,6 @@ const TabView = () => {
       <div>
         {tab === 'buy' && <BuyTab />}
         {tab === 'history' && <HistoryTab />}
-        {tab === 'usage' && <UsageTab />}
       </div>
     </>
   );
