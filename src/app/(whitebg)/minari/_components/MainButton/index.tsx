cd51d0915416c188022599abdea8d88997a818ca
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-12 01:03:03 +0900

    refactor: 분기 조건 수정

--- src/app/(whitebg)/minari/_components/MainButton/index.tsx
+++ src/app/(whitebg)/minari/_components/MainButton/index.tsx
@@ -6,26 +6,26 @@ import Button from '@/components/Button';
 import {useAnswerEligibility} from '@/hooks/queries/useAnswerEligibility';
 import {useRouter} from 'next/navigation';
 import {PATH} from '@/constants/path';
+import {useMemo} from 'react';
 
 const MainButton = () => {
   const {data} = useAnswerEligibility();
   const router = useRouter();
 
+  const isRetryAvailable = useMemo(() => {
+    return data === 'LIMIT_REACHED' || data === 'SEED_REQUIRED';
+  }, [data]);
+
   return (
     <div className={styles['button__wrapper']}>
-      {(data === 'LIMIT_REACHED' || data === 'SEED_REQUIRED') && (
+      {isRetryAvailable && (
         <Button border theme="secondary" onClick={() => router.push(PATH.REHEARSAL_RESULT)}>
           결과보기
         </Button>
       )}
-      <a href="/rehearsal" target="_blank" rel="noopener noreferrer">
-        <Button
-          border
-          iconRight={ArrowRight}
-          disabled={data === 'UNKNOWN' || data === 'SEED_REQUIRED'}
-          full
-        >
-          {data === 'LIMIT_REACHED' ? '다시 도전하기' : '면접 시작하기'}
+      <a href={PATH.REHEARSAL} target="_blank" rel="noopener noreferrer">
+        <Button border iconRight={ArrowRight} disabled={data === 'UNKNOWN'} full>
+          {isRetryAvailable ? '다시 도전하기' : '면접 시작하기'}
         </Button>
       </a>
     </div>
