ea4046c578f46232d813fc1be6cd7db6431a8c21
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-12 02:02:37 +0900

    refactor: 디테일 컴포넌트 내부로 옮김

--- src/app/(whitebg)/minari/page.tsx
+++ src/app/(whitebg)/minari/page.tsx
@@ -1,31 +1,23 @@
 import styles from './Minari.module.css';
 import Minari from '@/assets/minari-black.svg';
 import Image from 'next/image';
-import Shape from '@/assets/icon/shapes.svg';
 import Tags from './_components/Tags';
 import type {Metadata} from 'next';
 import MainButton from './_components/MainButton';
+import Detail from './_components/Detail';
 
 export const metadata: Metadata = {
   title: '미래의 나를 위한 리허설',
 };
 
 const MinariPage = () => {
-  const CATEGORY = '브라우저';
-
   return (
     <div className={styles.container}>
       <div className="title-lg">
         오늘의 미나리 <Image src={Minari} alt="" className="mg-top-4" width={28} />
       </div>
       <div className="title-xs mg-top-8 txt-secondary">지금 나에게 맞는 질문을 추천해드려요.</div>
-      <div className={styles['category__container']}>
-        <div>
-          <Image src={Shape} alt="shapes" />
-          <span className="label-lg txt-tertiary mg-left-4">세부 카테고리</span>
-        </div>
-        <div className={`label-lg txt-secondary ${styles.category}`}>{CATEGORY}</div>
-      </div>
+      <Detail />
       <Tags />
       <MainButton />
     </div>
