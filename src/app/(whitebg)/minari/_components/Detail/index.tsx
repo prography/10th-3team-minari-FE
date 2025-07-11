612b50097a4ccc7735132aa257cfb81aad7890ec
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-12 02:03:19 +0900

    feat: 태그 디테일 컴포넌트

---
+++ src/app/(whitebg)/minari/_components/Detail/index.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Image from 'next/image';
+import Shape from '@/assets/icon/shapes.svg';
+import styles from './Detail.module.css';
+import {useTagDetail} from '@/hooks/queries/useTagDetail';
+import {useQuestionId} from '@/hooks/queries/useQuestionId';
+
+const Detail = () => {
+  const {data: questionId} = useQuestionId();
+  const {data} = useTagDetail(questionId ?? 0);
+  return (
+    <div className={styles['category__container']}>
+      <div>
+        <Image src={Shape} alt="shapes" />
+        <span className="label-lg txt-tertiary mg-left-4">세부 카테고리</span>
+      </div>
+      {data && <div className={`label-lg txt-secondary ${styles.category}`}>{data.result}</div>}
+    </div>
+  );
+};
+
+export default Detail;
