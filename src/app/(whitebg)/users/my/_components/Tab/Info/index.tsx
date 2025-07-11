7d1e6c3b8b7dd02ebf3d7e0724e3ba63c7bf4945
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-12 01:22:29 +0900

    hotfix: 임시적으로 주석

--- src/app/(whitebg)/users/my/_components/Tab/Info/index.tsx
+++ src/app/(whitebg)/users/my/_components/Tab/Info/index.tsx
@@ -1,6 +1,6 @@
 import InfoBox from '@/components/InfoBox';
 import ProfileCard from '@/app/(whitebg)/users/my/_components/ProfileCard';
-import Heatmap from '@/app/(whitebg)/users/my/_components/Heatmap';
+// import Heatmap from '@/app/(whitebg)/users/my/_components/Heatmap';
 import {useUserHeatmapContext} from '@/contexts/UserHeatmapProvider';
 
 const InfoTab = () => {
@@ -16,13 +16,13 @@ const InfoTab = () => {
           days={userData?.dayCount}
         />
       </InfoBox>
-      <div className="mg-top-24 title-sm">{userData?.name}님의 미나리밭</div>
+      {/* <div className="mg-top-24 title-sm">{userData?.name}님의 미나리밭</div>
       <div className="body-md">
         내가 심은 날의 미나리를 클릭해서 나의 리허설 히스토리를 확인해요.
       </div>
       <div className="mg-top-20">
         <Heatmap />
-      </div>
+      </div> */}
     </div>
   );
 };
