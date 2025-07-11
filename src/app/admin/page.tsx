7da34f71b5088dc06543dc1676d3f7ffd7d4419a
Jiwon Choi <jiwon94k@gmail.com>
2025-07-11 10:37:39 +0900

    feat: [어드민] 어드민 진입 시 리워드지급 화면으로 자동 이동

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,25 +1,13 @@
 'use client';
-import Button from '@/components/Button';
 import {useRouter} from 'next/navigation';
+import {useEffect} from 'react';
 
 const AdminHome = () => {
-  const notReady = () => {
-    window.alert('준비중,,');
-  };
   const router = useRouter();
-  return (
-    <div style={{minHeight: 'calc(100dvh - 400px)'}}>
-      <div className="fx-center mg-top-32 title-md">
-        This is 어드민
-        <br />
-        Welcome 환영합니다 🥳
-      </div>
-      <div className="fx-center mg-top-32" style={{gap: '12px'}}>
-        <Button onClick={notReady}>유저 조회</Button>
-        <Button onClick={() => router.push('/admin/payments')}>리워드 지급</Button>
-      </div>
-    </div>
-  );
+  useEffect(() => {
+    router.push('/admin/payments');
+  }, []);
+  return <div style={{minHeight: 'calc(100dvh - 400px)'}}></div>;
 };
 
 export default AdminHome;
