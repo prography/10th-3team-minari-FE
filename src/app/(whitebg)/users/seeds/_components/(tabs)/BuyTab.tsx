0719d8eecdb0437f0dedf27ad77e84790f9672c5
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-11 18:20:46 +0900

    refactor: API 연동해서 API 값으로 보여주기

--- src/app/(whitebg)/users/seeds/_components/(tabs)/BuyTab.tsx
+++ src/app/(whitebg)/users/seeds/_components/(tabs)/BuyTab.tsx
@@ -3,45 +3,36 @@ import QRCodeModal from '../QRCodeModal';
 import Title from './(table)/Title';
 import Header from './(table)/Header';
 import Body, {type TbType} from './(table)/Body';
-import {useCallback} from 'react';
-import {TB_COUNT, TB_PRICE} from '@/constants/seedTable';
+import {useCallback, useMemo} from 'react';
 import Note from '../Note';
 import {NOTE_BUY} from '@/constants/note';
+import {useProductsSell} from '@/hooks/queries/useProductsSell';
 
 const BuyTab = () => {
   const {open} = useModalStore();
+  const {data} = useProductsSell();
 
-  const discount = 50;
-  const day = '2025.07.26';
-
-  const DiscountFiled = (): TbType => {
-    if (discount != null && day != null) {
-      return {type: 'text-red', text: `${day}까지 ${discount}% 할인`};
-    }
-    return {type: 'text-disabled', text: `지금은 이벤트가 없습니다`};
-  };
+  const initialData = useMemo<TbType[][]>(() => [[{type: 'text', text: ''}]], []);
 
   const TbBuyDatasMake = useCallback((): TbType[][] => {
-    return Array.from({length: 5}, () => []).map((_, index) => {
-      const count = TB_COUNT[index];
-      const price = TB_PRICE[index];
-      const discountedPrice = price * (1 - discount / 100);
-
-      return [
-        {type: 'text', text: '씨앗'},
-        {type: 'text', text: `${count}개`},
-        {type: 'text', text: `${price}원`},
-        DiscountFiled(),
-        {
-          type: 'button',
-          text: `${discountedPrice}원`,
-          onClick: () => {
-            open(<QRCodeModal />, false, true);
-          },
-        },
-      ];
-    });
-  }, [open]);
+    return data
+      ? data.map((value) => {
+          return [
+            {type: 'text', text: '씨앗'},
+            {type: 'text', text: `${value.quantity}개`},
+            {type: 'text', text: `${value.realPrice}원`},
+            {type: 'text-red', text: `${value.message}`},
+            {
+              type: 'button',
+              text: `${value.fakePrice}원`,
+              onClick: () => {
+                open(<QRCodeModal />, false, true);
+              },
+            },
+          ];
+        })
+      : initialData;
+  }, [open, data, initialData]);
 
   return (
     <>
