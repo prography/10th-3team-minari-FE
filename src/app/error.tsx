e1f4f0488bdf9735614b7f816ad91ee00a7bacef
Jiwon Choi <jiwon94k@gmail.com>
2025-07-12 03:47:03 +0900

    fix: 에러페이지 문구 수정내용 반영

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,20 @@
 'use client';
 import ErrorLayout from '@/components/ErrorLayout';
+import Button from '@/components/Button';
+import {OUT_LINK} from '@/constants/path';
 
 export default function ServerErrorPage() {
   return (
     <ErrorLayout code={500}>
-      <div className="title-sm">일시적인 오류가 발생했어요</div>
-      <div className="title-sm">
-        잠시 후 다시 시도하거나, 문제가 계속되면 관리자에게 문의해주세요.
+      <div className="title-sm">우와, 아직 심어지지 못한 미나리를 발견하셨군요!</div>
+      <div className="title-sm">어떻게 이 미나리를 찾았는지 말씀해주실래요?</div>
+      <div className="fx-align-center mg-top-20" style={{gap: '16px'}}>
+        <Button theme="secondary" border onClick={() => window.open(OUT_LINK.FAQ)}>
+          제보하기
+        </Button>
+        <Button border onClick={() => window.history.back()}>
+          돌아가기
+        </Button>
       </div>
     </ErrorLayout>
   );
