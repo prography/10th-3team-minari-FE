b9b27f9436013d366681bfdb9917b3c268600858
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-11 17:43:07 +0900

    refactor: infobox 내부로 옮기기

--- src/app/(whitebg)/users/seeds/page.tsx
+++ src/app/(whitebg)/users/seeds/page.tsx
@@ -1,15 +1,10 @@
-'use client';
-
 import styles from './Seeds.module.css';
 import TabView from './_components/(tabs)/TabView';
-import InfoBox from '@/components/InfoBox';
-import {useUsers} from '@/hooks/queries/useUsers';
 import Image from 'next/image';
 import Logo from '@/assets/minari-black.svg';
+import SeedInfoBox from './_components/SeedInfoBox';
 
 const SeedsLayout = () => {
-  const {data} = useUsers();
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>
@@ -18,15 +13,7 @@ const SeedsLayout = () => {
       </div>
 
       <div className={styles.content}>
-        <InfoBox>
-          <div className={styles.seeds_info}>
-            <span className="title-sm txt-white">씨앗 이용 정보</span>
-            <div className={`${styles.seeds_info_flex} body-lg txt-white`}>
-              <span>{`${data ? data.name : ''}님의 씨앗`}</span>
-              <span>{`${data ? data.seed : 0} 개`}</span>
-            </div>
-          </div>
-        </InfoBox>
+        <SeedInfoBox />
 
         <TabView />
       </div>
