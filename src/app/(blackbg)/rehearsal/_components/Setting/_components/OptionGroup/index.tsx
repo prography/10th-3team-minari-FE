cc236f95d8b233a49037e7db50282aaf9edcd9c1
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-12 01:02:36 +0900

    feat: 씨앗 상태에따라 모달 or 리허설 페이지로 이동

--- src/app/(blackbg)/rehearsal/_components/Setting/_components/OptionGroup/index.tsx
+++ src/app/(blackbg)/rehearsal/_components/Setting/_components/OptionGroup/index.tsx
@@ -7,11 +7,19 @@ import {DeviceSelectProvider} from '@/contexts/DeviceSelectProvider';
 import {useRehearsal} from '@/contexts/RehearsalProvider';
 import {useEffect, useState} from 'react';
 import {useMediaStore} from '@/stores/mediaStore';
+import {useAnswerEligibility} from '@/hooks/queries/useAnswerEligibility';
+import {useModalStore} from '@/stores/modalStore';
+import {PATH} from '@/constants/path';
+import Modal from '@/components/Modal';
+import {useRouter} from 'next/navigation';
 
 const OptionGroup = () => {
+  const {data} = useAnswerEligibility();
   const {handleIsReharsal} = useRehearsal();
   const {mediaStreamStatus} = useMediaStore();
   const [disabled, setDisabled] = useState(true);
+  const {open: opneModal, close: closeModal} = useModalStore();
+  const router = useRouter();
 
   useEffect(() => {
     if (mediaStreamStatus === 'connected') {
@@ -23,6 +31,23 @@ const OptionGroup = () => {
     handleIsReharsal();
   };
 
+  const handleBuySeeds = () => {
+    router.push(PATH.UESRS_SEEDS);
+    closeModal();
+  };
+
+  const handleClickOpenModal = () => {
+    opneModal(
+      <Modal
+        title={'앗 씨앗이 부족해요.'}
+        rightButton={<Button onClick={handleBuySeeds}>{'씨앗 사러 가기'}</Button>}
+      >
+        <p>더 멋진 답변을 준비할 수 있어요.</p>
+        <p>{'씨앗을 사러 가볼까요?'}</p>
+      </Modal>,
+    );
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles['select-group']}>
@@ -31,7 +56,15 @@ const OptionGroup = () => {
           <MicSelectOption />
         </DeviceSelectProvider>
       </div>
-      <Button onClick={handleRehearsalStart} iconLeft={ArrowBlack} disabled={disabled}>
+      <Button
+        onClick={
+          data === 'UNKNOWN' || data === 'LIMIT_REACHED'
+            ? handleClickOpenModal
+            : handleRehearsalStart
+        }
+        iconLeft={ArrowBlack}
+        disabled={disabled}
+      >
         면접 시작하기
       </Button>
     </div>
