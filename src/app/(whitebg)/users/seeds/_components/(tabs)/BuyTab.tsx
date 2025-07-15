682db1cc1bfa8b26307d0d785e5f65cd884f539f
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-15 16:11:50 +0900

    refactor: 구매 모달 문구 수정

--- src/app/(whitebg)/users/seeds/_components/(tabs)/BuyTab.tsx
+++ src/app/(whitebg)/users/seeds/_components/(tabs)/BuyTab.tsx
@@ -7,10 +7,22 @@ import {useCallback, useMemo} from 'react';
 import Note from '../Note';
 import {NOTE_BUY} from '@/constants/note';
 import {useProductsSell} from '@/hooks/queries/useProductsSell';
+import {useUsers} from '@/hooks/queries/useUsers';
 
 const BuyTab = () => {
   const {open} = useModalStore();
   const {data} = useProductsSell();
+  const {data: userData} = useUsers();
+
+  const order = useMemo(
+    () => [
+      `송금 메모: UUID(${userData?.uuid ?? ''}) 적기!`,
+      '토스뱅크 1002-498-076593으로',
+      '송금 완료 화면 캡쳐하기!',
+      '위 QR 스캔하고 사진 두 개 첨부해 보내기!',
+    ],
+    [userData],
+  );
 
   const initialData = useMemo<TbType[][]>(() => [[{type: 'text', text: ''}]], []);
 
@@ -26,13 +38,22 @@ const BuyTab = () => {
               type: 'button',
               text: `${value.fakePrice}원`,
               onClick: () => {
-                open(<QRCodeModal />, false, true);
+                open(
+                  <QRCodeModal
+                    order={order}
+                    uuid={userData?.uuid ?? ''}
+                    title="원하는 씨앗을 선택하셨나요?"
+                    subTitle="최대 1시간 이내로 씨앗을 보내드릴게요."
+                  />,
+                  false,
+                  true,
+                );
               },
             },
           ];
         })
       : initialData;
-  }, [open, data, initialData]);
+  }, [open, data, initialData, order, userData]);
 
   return (
     <>
