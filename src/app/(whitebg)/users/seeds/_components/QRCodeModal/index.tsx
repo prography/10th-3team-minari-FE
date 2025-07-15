5803fd22d05739e7e79d96e36df07909e5a17c7d
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-15 16:10:46 +0900

    refactor: props로 받아서 사용하도록 수정

--- src/app/(whitebg)/users/seeds/_components/QRCodeModal/index.tsx
+++ src/app/(whitebg)/users/seeds/_components/QRCodeModal/index.tsx
@@ -3,34 +3,24 @@ import styles from './QRCodeModal.module.css';
 import QRCode from '@/assets/qr-code.jpeg';
 import Image from 'next/image';
 import X from '@/assets/icon/x.svg';
-import Round1 from '@/assets/icon/round-1.svg';
-import Round2 from '@/assets/icon/round-2.svg';
-import Round3 from '@/assets/icon/round-3.svg';
-import Round4 from '@/assets/icon/round-4.svg';
 import {useModalStore} from '@/stores/modalStore';
+import {ROUND_ICON} from '@/constants/roundIcon';
 
-const QRCodeModal = () => {
+interface QRCodeModalProps {
+  order: string[];
+  uuid: string;
+  title: string;
+  subTitle: string;
+}
+
+const QRCodeModal = ({order, uuid, title, subTitle}: QRCodeModalProps) => {
   const id = useId();
   const {close} = useModalStore();
 
-  const order = [
-    {
-      icon: Round1,
-      text: '송금 메모: ⭐️UUID⭐ 적기!',
-    },
-    {
-      icon: Round2,
-      text: '토스뱅크 1002-498-076593으로',
-    },
-    {
-      icon: Round3,
-      text: '송금 완료 화면 캡쳐하기!',
-    },
-    {
-      icon: Round4,
-      text: '위 QR 스캔하고 사진 두 개 첨부해 보내기!',
-    },
-  ];
+  const viewOrder = order.map((text, index) => ({
+    icon: ROUND_ICON[index + 1],
+    text,
+  }));
 
   return (
     <div className={styles.position}>
@@ -38,17 +28,17 @@ const QRCodeModal = () => {
         <button className={styles.close_button} onClick={() => close()}>
           <Image src={X} alt="종료버튼" width={24} height={24} />
         </button>
-        <span className="title-xs txt-primary">원하는 씨앗을 선택하셨나요?</span>
-        <span className="body-lg">최대 1시간 이내로 씨앗을 보내드릴게요.</span>
-        <Image src={QRCode} alt="QR code" width={138} height={138} />
+        <span className="title-xs txt-primary">{title}</span>
+        <span className="body-lg">{subTitle}</span>
 
-        <span
-          style={{whiteSpace: 'pre'}}
-          className="label-sm txt-tertiary"
-        >{`UUID: 홈 → 오른쪽 상단 [프로필 사진] \n→ 내 이름 옆 글자 (ex. 귀여운사자87)`}</span>
+        <div className="fx-col g-12 fx-center">
+          <Image src={QRCode} alt="QR code" width={138} height={138} />
+
+          <span className="body-sm">{`당신의 UUID: ${uuid}`}</span>
+        </div>
 
         <ol className={styles.order_list}>
-          {order.map(({icon, text}, i) => (
+          {viewOrder.map(({icon, text}, i) => (
             <li key={`${id}_${i}`} className={`${styles.order_item} label-md`}>
               <Image src={icon} alt="icon" aria-hidden />
               <span>{text}</span>
