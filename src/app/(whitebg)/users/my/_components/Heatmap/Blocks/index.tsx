67dd3955fda20e524441d11c015882934c6f81a1
Jiwon Choi <jiwon94k@gmail.com>
2025-07-15 20:48:32 +0900

    feat: [마이페이지] 잔디 월 단위 추가

--- src/app/(whitebg)/users/my/_components/Heatmap/Blocks/index.tsx
+++ src/app/(whitebg)/users/my/_components/Heatmap/Blocks/index.tsx
@@ -1,43 +1,49 @@
 import styles from './Blocks.module.css';
 import {useUserHeatmapContext} from '@/contexts/UserHeatmapProvider';
-import Check from '@/assets/icon/check.svg';
-import Image from 'next/image';
 import {Tooltip} from 'react-tooltip';
-import {useDate} from '@/hooks/useDate';
+import Block from '@/app/(whitebg)/users/my/_components/Heatmap/Block';
+import {weekLabel} from '@/constants/dates';
 
 const Blocks = () => {
-  const {blocks, onClickBlock} = useUserHeatmapContext();
-  const {dateFormatterKorean} = useDate();
-  const weekLabel = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+  const {blocks, monthBlocks, onClickBlock, heatmapTab} = useUserHeatmapContext();
 
   return (
     <div className={styles.container}>
-      <div className={styles['week-month-label']}>
-        {weekLabel.map((label, index) => (
-          <div key={index} style={{width: '14%'}}>
-            <span className="label-sm txt-tertiary">{label}</span>
+      {heatmapTab === 1 && (
+        <div className={styles['week-number-label']}>
+          <span className="txt-tertiary label-sm">1주</span>
+          <span className="txt-tertiary label-sm">3주</span>
+          <span className="txt-tertiary label-sm">5주</span>
+        </div>
+      )}
+      <div className={styles['blocks__area']}>
+        <div className={styles['week-month-label']}>
+          {weekLabel.map((label, index) => (
+            <div key={index} style={{width: '14%'}}>
+              <span className="label-sm txt-tertiary">{label}</span>
+            </div>
+          ))}
+        </div>
+        <Tooltip id="minari-tooltip" opacity={1} style={{borderRadius: '8px'}} openOnClick />
+        {heatmapTab === 1 &&
+          monthBlocks.map((weekBlock, index) => (
+            <div className={styles['blocks__wrap']} key={index}>
+              {weekBlock.map((block, index) => (
+                <div key={index} style={{width: '14%'}}>
+                  <Block block={block} onClickBlock={onClickBlock} />
+                </div>
+              ))}
+            </div>
+          ))}
+        {heatmapTab === 2 && (
+          <div className={styles['blocks__wrap']}>
+            {blocks?.map((block, index) => (
+              <div key={index} style={{width: '14%'}}>
+                <Block block={block} onClickBlock={onClickBlock} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <Tooltip id="minari-tooltip" opacity={1} style={{borderRadius: '8px'}} openOnClick />
-      <div className={styles['blocks__wrap']}>
-        {blocks?.map((block, index) => (
-          <div
-            key={index}
-            className={`${styles.blocks} ${block.done ? `${styles.done}` : ''} ${block.active ? `${styles.active}` : ''}`}
-            style={{width: '14%'}}
-            onClick={() => {
-              onClickBlock(block);
-            }}
-          >
-            <a
-              data-tooltip-id="minari-tooltip"
-              data-tooltip-content={dateFormatterKorean(block.date)}
-            >
-              {block.active && <Image src={Check} alt="" />}
-            </a>
-          </div>
-        ))}
+        )}
       </div>
     </div>
   );
--- src/app/(whitebg)/users/my/_components/Heatmap/index.tsx
+++ src/app/(whitebg)/users/my/_components/Heatmap/index.tsx
@@ -3,11 +3,13 @@ import Blocks from '@/app/(whitebg)/users/my/_components/Heatmap/Blocks';
 import BlockDetail from '@/app/(whitebg)/users/my/_components/Heatmap/BlockDetail';
 import {useUserHeatmapContext} from '@/contexts/UserHeatmapProvider';
 import Loader from '@/components/Loader';
+import BlockTab from '@/app/(whitebg)/users/my/_components/Heatmap/BlockTab';
 
 const Heatmap = () => {
   const {mapLoading} = useUserHeatmapContext();
   return (
     <div>
+      <BlockTab />
       {mapLoading ? (
         <div className="mg-top-32" style={{height: '100px'}}>
           <Loader />
