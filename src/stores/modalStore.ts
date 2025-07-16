c50353531f840998f1e1baee9000b69e9a09905f
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-17 00:08:07 +0900

    refactor: open 매개변수 객체로 수정, 뒷배경 클릭시 동작할 콜백 함수 추가

--- src/stores/modalStore.ts
+++ src/stores/modalStore.ts
@@ -1,27 +1,39 @@
 import {create} from 'zustand';
 
+interface OpenType {
+  modal: React.ReactNode;
+  disableBackdropClick?: boolean;
+  historyStackPush?: boolean;
+  onBackdropClick?: () => void;
+}
+
 interface ModalStore {
   modal: React.ReactNode | null;
   isOpen: boolean;
-  open: (
-    modal: React.ReactNode,
-    disableBackdropClick?: boolean,
-    historyStackPush?: boolean,
-  ) => void;
+  open: ({modal, disableBackdropClick, historyStackPush, onBackdropClick}: OpenType) => void;
   close: () => void;
   change: (modal: React.ReactNode) => void;
   disableBackdropClick: boolean;
   historyStackPush: boolean;
+  onBackdropClick?: () => void;
 }
 
 export const useModalStore = create<ModalStore>((set) => ({
   modal: null,
   isOpen: false,
-  open: (modal, disableBackdropClick = false, historyStackPush = false) =>
-    set({isOpen: true, modal, disableBackdropClick, historyStackPush}),
-  close: () =>
-    set({isOpen: false, modal: null, disableBackdropClick: false, historyStackPush: false}),
+  open: ({modal, disableBackdropClick = false, historyStackPush = false, onBackdropClick}) =>
+    set({isOpen: true, modal, disableBackdropClick, historyStackPush, onBackdropClick}),
+  close: () => {
+    set({
+      isOpen: false,
+      modal: null,
+      disableBackdropClick: false,
+      historyStackPush: false,
+      onBackdropClick: undefined,
+    });
+  },
   change: (modal) => set((state) => (state.isOpen ? {...state, modal} : state)),
   disableBackdropClick: false,
   historyStackPush: false,
+  onBackdropClick: undefined,
 }));
