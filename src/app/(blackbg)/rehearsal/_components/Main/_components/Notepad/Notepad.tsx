dddce01a77c0eb612d134f13ac0b7bdf79df1e8c
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-22 21:17:05 +0900

    refactor: 메모 타이틀 추가 디자인 수정

--- src/app/(blackbg)/rehearsal/_components/Main/_components/Notepad/Notepad.tsx
+++ src/app/(blackbg)/rehearsal/_components/Main/_components/Notepad/Notepad.tsx
@@ -1,20 +1,23 @@
 import {useNotepad} from '@/contexts/NotepadProvider';
 import styles from './Notepad.module.css';
+import Image from 'next/image';
+import Pen from '@/assets/icon/pen-white.svg';
 
 const Notepad = () => {
-  const {open, memo, handleChange} = useNotepad();
+  const {memo, handleChange} = useNotepad();
 
   return (
-    <>
-      {open && (
-        <textarea
-          className={`${styles.textarea} body-lg`}
-          placeholder="메모를 입력해주세요."
-          value={memo}
-          onChange={handleChange}
-        />
-      )}
-    </>
+    <div className={styles.wrapper}>
+      <span className={`${styles.title} label-lg txt-white`}>
+        <Image src={Pen} alt="펜 모양 아이콘" /> 메모
+      </span>
+      <textarea
+        className={`${styles.textarea} body-lg txt-white`}
+        placeholder="이 공간을 컨닝페이퍼처럼 사용해보세요!"
+        value={memo}
+        onChange={handleChange}
+      />
+    </div>
   );
 };
 
