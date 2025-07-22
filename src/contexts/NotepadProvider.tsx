648695283c87e97645519369f4b3e283cfb449d7
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-22 21:16:29 +0900

    refactor: open 관련 상태 제거

--- src/contexts/NotepadProvider.tsx
+++ src/contexts/NotepadProvider.tsx
@@ -1,28 +1,21 @@
 import {createContext, useContext, useMemo, useState} from 'react';
 
 type ContextType = {
-  open: boolean;
   memo: string;
-  handleOpen: () => void;
   handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 };
 
 const NotepadContext = createContext<ContextType | null>(null);
 
 export const NotepadProvider = ({children}: {children: React.ReactNode}) => {
-  const [open, setOpen] = useState(false);
   const [memo, setMemo] = useState('');
-
-  const handleOpen = () => {
-    setOpen((prev) => !prev);
-  };
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     e.preventDefault();
     setMemo(e.target.value);
   };
 
-  const value = useMemo(() => ({open, memo, handleOpen, handleChange}), [open, memo]);
+  const value = useMemo(() => ({memo, handleChange}), [memo]);
 
   return <NotepadContext.Provider value={value}>{children}</NotepadContext.Provider>;
 };
