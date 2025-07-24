8d3c13de34e451612b8fb430ff034627b1bf3725
Jiwon Choi <jiwon94k@gmail.com>
2025-07-24 16:43:08 +0900

    feat: [메인] 카테고리 / 키워드 디자인 변경내용 반영

--- src/app/(whitebg)/minari/_components/Detail/index.tsx
+++ src/app/(whitebg)/minari/_components/Detail/index.tsx
@@ -15,7 +15,7 @@ const Detail = () => {
         <Image src={Shape} alt="shapes" />
         <span className="label-lg txt-tertiary mg-left-4">세부 카테고리</span>
       </div>
-      {data && <div className={`label-lg txt-secondary ${styles.category}`}>{data.result}</div>}
+      {data && <div className="label-lg txt-secondary">{data.result}</div>}
     </div>
   );
 };
