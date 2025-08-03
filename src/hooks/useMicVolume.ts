0605619fada6a9009844deb618fb7741d394f04d
_wo_.ong <ckehfqkr302@gmail.com>
2025-08-04 00:43:48 +0900

    refactor: requestAnimationFrame으로 애니메이션 성능 개선

--- src/hooks/useMicVolume.ts
+++ src/hooks/useMicVolume.ts
@@ -1,7 +1,7 @@
 'use client';
 
 import {useMediaStore} from '@/stores/mediaStore';
-import {useEffect, useRef, useState} from 'react';
+import {useEffect, useState} from 'react';
 
 /**
  * source - 마이크 입력을 오디오 처리 파이프라인에 넣는 시작점
@@ -11,7 +11,7 @@ import {useEffect, useRef, useState} from 'react';
 const useMicVolume = () => {
   const [volume, setVolume] = useState(0);
   const {mediaStream} = useMediaStore();
-  const intervalId = useRef<NodeJS.Timer | number>(0);
+  // intervalId ref removed
 
   useEffect(() => {
     if (!mediaStream) return;
@@ -23,36 +23,32 @@ const useMicVolume = () => {
     source.connect(analyser);
 
     const dataArray = new Float32Array(analyser.fftSize);
+    let animationFrameId: number;
 
-    const startVolumeMonitoring = () => {
-      intervalId.current = setInterval(() => {
-        analyser.getFloatTimeDomainData(dataArray); // 마이크 오디오 파형(-1.0 ~ 1.0) 추출
+    const measureVolume = () => {
+      analyser.getFloatTimeDomainData(dataArray); // 마이크 오디오 파형(-1.0 ~ 1.0) 추출
 
-        const sumSquares = dataArray.reduce((sum, value) => sum + value * value, 0); //각 샘플 값 제곱으로 더해서 음수가 있어도 양수로 표현
+      const sumSquares = dataArray.reduce((sum, value) => sum + value * value, 0); //각 샘플 값 제곱으로 더해서 음수가 있어도 양수로 표현
+      const RMS = Math.sqrt(sumSquares / dataArray.length); //Root Mean Square값 소리 크기의 대표값
 
-        const RMS = Math.sqrt(sumSquares / dataArray.length); //Root Mean Square값 소리 크기의 대표값
+      const weight = 10;
+      const amplifiedVolume = RMS * weight; // 너무 작은 RMS값을 사람이 보기 좋은 수준으로 증폭
+      const normalizedVolume = Math.min(Math.round(amplifiedVolume * 100), 100); // 0~100범위로 정규화
 
-        const weight = 10;
+      setVolume((prev) => {
+        if (Math.abs(prev - normalizedVolume) > 2) {
+          return normalizedVolume;
+        }
+        return prev;
+      });
 
-        const amplifiedVolume = RMS * weight; // 너무 작은 RMS값을 사람이 보기 좋은 수준으로 증폭
-
-        const normalizedVolume = Math.min(Math.round(amplifiedVolume * 100), 100); // 0~100범위로 정규화
-
-        setVolume((prev) => {
-          if (Math.abs(prev - normalizedVolume) > 2) {
-            return normalizedVolume;
-          }
-          return prev;
-        });
-      }, 100);
+      animationFrameId = requestAnimationFrame(measureVolume);
     };
 
-    startVolumeMonitoring();
+    animationFrameId = requestAnimationFrame(measureVolume);
 
     return () => {
-      if (intervalId.current) {
-        clearInterval(intervalId.current as number);
-      }
+      cancelAnimationFrame(animationFrameId);
       source.disconnect();
       analyser.disconnect();
       void audioCtx.close();
