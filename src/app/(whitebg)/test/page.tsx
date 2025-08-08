70320c62b13828153d990b7c40b5614cdffaf3c8
_wo_.ong <ckehfqkr302@gmail.com>
2025-08-08 14:27:02 +0900

    refactor: props수정

--- src/app/(whitebg)/test/page.tsx
+++ src/app/(whitebg)/test/page.tsx
@@ -42,7 +42,14 @@ const TestPage = () => {
 
   const tossModal = () => {
     opneModal({
-      modal: <TossPaymentWidget />,
+      modal: (
+        <TossPaymentWidget
+          orderId="_GRKrwUl-Tslbgin660fW"
+          orderName="토스 티셔츠 외 2건"
+          price={100}
+          customerKey="IC4jzlhz7Axz_FW7qVu-l"
+        />
+      ),
     });
   };
 
