3d2454914a9874ce63fa3faf2f10c661a2e7ea5e
_wo_.ong <ckehfqkr302@gmail.com>
2025-08-09 12:07:15 +0900

    refactor: 리허설 페이지 반응형 추가

--- src/app/(whitebg)/rehearsal/result/_components/Keyword/index.tsx
+++ src/app/(whitebg)/rehearsal/result/_components/Keyword/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Keywrod.module.css';
 
 const Keywrod = ({children}: {children: React.ReactNode}) => {
-  return <div className={`${styles.wrapper} label-lg txt-brand`}>{children}</div>;
+  return (
+    <div className={`${styles.wrapper} label-lg txt-brand`}>
+      <span className={styles.text}>{children}</span>
+    </div>
+  );
 };
 
 export default Keywrod;
