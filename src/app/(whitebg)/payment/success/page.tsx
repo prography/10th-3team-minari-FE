5ff42be77663ee2155e0fb7167b4d16d11fb50d4
_wo_.ong <ckehfqkr302@gmail.com>
2025-08-11 00:41:12 +0900

    feat: payment 성공, 실패 페이지 추가

--- src/app/(whitebg)/payment/success/page.tsx
+++ src/app/(whitebg)/payment/success/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const page = () => {
+const PaymentSuccessPage = () => {
   return (
     <div>
-      <h1>결제에 성공했어요</h1>
+      <h1>결제에 성공했어요.</h1>
     </div>
   );
 };
 
-export default page;
+export default PaymentSuccessPage;
