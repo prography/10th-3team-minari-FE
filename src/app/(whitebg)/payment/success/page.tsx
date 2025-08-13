eb4713fb7ddefb690192a56d5f58049254031d8f
_wo_.ong <ckehfqkr302@gmail.com>
2025-08-13 21:02:10 +0900

    feat: get API 임시 연동

--- src/app/(whitebg)/payment/success/page.tsx
+++ src/app/(whitebg)/payment/success/page.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+'use client';
 
-const PaymentSuccessPage = () => {
+import {useTossPayments} from '@/hooks/queries/useTossPayments';
+import {useRouter, useSearchParams} from 'next/navigation';
+import {Suspense, useEffect} from 'react';
+
+const PaymentSuccessInner = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const paymentKey = searchParams.get('paymentKey') ?? '';
+
+  const {isLoading, error} = useTossPayments(paymentKey);
+
+  useEffect(() => {
+    if (!paymentKey) {
+      router.replace('/payment/fail');
+    }
+  }, [router, paymentKey]);
+
+  if (isLoading) return <div>로딩중...</div>;
+  if (error) return <div>결제 정보 불러오기 실패</div>;
+
   return (
     <div>
       <h1>결제에 성공했어요.</h1>
@@ -8,4 +27,10 @@ const PaymentSuccessPage = () => {
   );
 };
 
-export default PaymentSuccessPage;
+export default function PaymentSuccessPage() {
+  return (
+    <Suspense fallback={<div>로딩중...</div>}>
+      <PaymentSuccessInner />
+    </Suspense>
+  );
+}
