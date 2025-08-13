2aeebda873815d34fb668355b3763576bae65dcf
_wo_.ong <ckehfqkr302@gmail.com>
2025-08-13 21:01:57 +0900

    feat: 페이먼츠 결과 받을수 있도록 getAPI 추가

--- src/apis/toss.ts
+++ src/apis/toss.ts
@@ -18,15 +18,63 @@ export const postTossPaymentConfirm = async (data: TossPaymentConfirmPostType) =
   }
 };
 
+export const getTossPayments = async ({paymentKey}: TossPaymentsGetType) => {
+  try {
+    const response = await fetch.get<TossPaymentsResponseType>(`/toss/payments/${paymentKey}`);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
 interface TossPayment {
   orderId: string;
   amount: number;
-}
-
-export interface TossPaymentPreparePostType extends TossPayment {
   productId: number;
 }
 
-export interface TossPaymentConfirmPostType extends TossPayment {
+interface TossPaymentKey {
   paymentKey: string;
+}
+
+type TossPaymentPreparePostType = TossPayment;
+
+type TossPaymentConfirmPostType = TossPayment & TossPaymentKey;
+
+type TossPaymentsGetType = TossPaymentKey;
+
+interface TossPaymentsResponseType {
+  version: string;
+  paymentKey: string;
+  type: string;
+  orderId: string;
+  orderName: string;
+  mId: string;
+  currency: string;
+  method: string;
+  totalAmount: number;
+  balanceAmount: number;
+  status: string;
+  requestedAt: string;
+  approvedAt: string;
+  useEscrow: boolean;
+  cultureExpense: boolean;
+  isPartialCancelable: boolean;
+  suppliedAmount: number;
+  vat: number;
+  taxFreeAmount: number;
+  taxExemptionAmount: number;
+  lastTransactionKey: string;
+  country: string;
+  easyPay: {
+    provider: string;
+    amount: number;
+    discountAmount: number;
+  };
+  receipt: {
+    url: string;
+  };
+  checkout: {
+    url: string;
+  };
 }
