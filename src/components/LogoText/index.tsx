fe1daa8365e12f6350726d805cacbe952f84f054
_wo_.ong <ckehfqkr302@gmail.com>
2025-08-16 14:51:30 +0900

    feat: 로고텍스트 as 키워드로 태그 추가할 수 있도록 확장

--- src/components/LogoText/index.tsx
+++ src/components/LogoText/index.tsx
@@ -3,18 +3,26 @@ import React from 'react';
 import Logo from '@/assets/minari-black.svg';
 import styles from './LogoText.module.css';
 
-interface LogoTextProps extends React.HTMLAttributes<HTMLSpanElement> {
+type LogoTextProps<T extends React.ElementType = 'span'> = React.ComponentPropsWithoutRef<T> & {
+  as?: T;
   children: React.ReactNode;
   logoSize?: number;
-}
+};
 
-const LogoText = ({children, logoSize = 20, ...props}: LogoTextProps) => {
+const LogoText = <T extends React.ElementType = 'span'>({
+  as,
+  children,
+  logoSize = 20,
+  ...props
+}: LogoTextProps<T>) => {
+  const Component = as || 'div';
+
   return (
     <div className={styles.wrapper}>
       <Image src={Logo} alt="로고" width={logoSize} height={logoSize} />
-      <span {...props} className={props.className ? props.className : 'txt-primary body-md'}>
+      <Component {...props} className={props.className ? props.className : 'txt-primary body-md'}>
         {children}
-      </span>
+      </Component>
     </div>
   );
 };
